e90e094ac672d4868a3f633a19d2ef13423fedcf
msaltieri <michelestefano.altieri@gmail.com>
2017-04-04 13:01:44 +0200

    Create the `game` S3 class

---
+++ R/game-attribs.R
@@ -0,0 +1,77 @@
+#' Get the game status (number of fingers alive)
+#'
+#' @param game An \code{alicetwist} game object
+#' @return A matrix representing the current state
+#' of the game (number of fingers still alive) with
+#' the players on the rows and the hands on the columns
+#' @examples
+#' game <- new_game()
+#' game
+#' game_status(game)
+#' @export
+game_status <- function(game) {
+  stopifnot(inherits(game, "alicetwist"))
+  game[["status"]]
+}
+
+#' Set the game status
+#'
+#' @param game An \code{alicetwist} game object
+#' @param player The player whose hands are to
+#' be modified
+#' @param values A vector containing the new
+#' number of fingers to assign to each hand
+#' @export
+#' @keywords internal
+`game_status<-` <- function(game, player, values) {
+  stopifnot(inherits(game, "alicetwist"))
+  if (player > game[["players"]])
+    stop("`player` must be at least equal to the number of players in the game",
+         .call = FALSE)
+  game[["status"]][player, ] <- values
+  game
+}
+
+#' Get the number of players
+#'
+#' @param game An \code{alicetwist} game object
+#' @export
+#' @keywords internal
+game_players <- function(game) {
+  stopifnot(inherits(game, "alicetwist"))
+  game[["players"]]
+}
+
+#' Get the number of hands
+#'
+#' @param game An \code{alicetwist} game object
+#' @export
+#' @keywords internal
+game_hands <- function(game) {
+  stopifnot(inherits(game, "alicetwist"))
+  game[["hands"]]
+}
+
+#' Get the number of fingers
+#'
+#' @param game An \code{alicetwist} game object
+#' @export
+#' @keywords internal
+game_fingers <- function(game) {
+  stopifnot(inherits(game, "alicetwist"))
+  game[["fingers"]]
+}
+
+#' Is the game allowing the split move?
+#'
+#' Split move allows players who have zero
+#' fingers left in one hand to move fingers
+#' from other hands.
+#'
+#' @param game An \code{alicetwist} game object
+#' @export
+#' @keywords internal
+game_split <- function(game) {
+  stopifnot(inherits(game, "alicetwist"))
+  game[["split"]]
+}
---
+++ R/game.R
@@ -0,0 +1,83 @@
+#' Initialize a new Alice Twist game
+#'
+#' Create an Alice Twist game that can
+#' be played by two users or against
+#' the CPU.
+#'
+#' @param players The number of players.
+#' \code{players} at the moment can be
+#' between 2 and 4 even if some methods
+#' are built for two players only.
+#' @param hands The number of hands for
+#' each player. Since this implementation
+#' come from the original kids game, the
+#' buckets that each player has are call
+#' hands. \code{hands} must be integer
+#' but at the moment its number has no
+#' limit.
+#' @param fingers The number of fingers
+#' for each hand. Also in this case the
+#' name of the variable is derived from
+#' the kids game but in this general form
+#' the number of \code{fingers} could be
+#' different from 5, provided that it
+#' must be integer.
+#' @param split If \code{TRUE}, then
+#' every time a split is possibile during
+#' the game it is performed. If \code{FALSE}
+#' the split is never performed.
+#' @return A \code{alicetwist} game object.
+#' @examples
+#' new_game()
+#' @export
+new_game <- function(players = 2,
+                     hands = 2,
+                     fingers = 5,
+                     split = TRUE) {
+
+  # Make sure the number of players is integer
+  if (players %% 1 != 0)
+    stop("number of `players` must be integer",
+         .call = FALSE)
+
+  # There must be at least 2 players
+  if (players < 2)
+    stop("number of `players` must be at least 2",
+         call. = FALSE)
+
+  # Make sure the number of hands is integer
+  if (hands %% 1 != 0)
+    stop("number of `hands` must be integer",
+         .call = FALSE)
+
+  # There must be at least 2 hands
+  if (hands < 2)
+    stop("number of `hands` must be at least 2",
+         call. = FALSE)
+
+  # Make sure the number of fingers is integer
+  if (fingers %% 1 != 0)
+    stop("number of `fingers` must be integer",
+         .call = FALSE)
+
+  # There must be at least 3 fingers
+  if (finger < 3)
+    stop("number of `fingers` must be at least 3",
+         call. = FALSE)
+
+  # Generate the initial status
+  game <- list(
+    status = matrix(data = 1,
+                    nrow = players,
+                    ncol = hands),
+    players = players,
+    hands = hands,
+    fingers = fingers,
+    split = split
+  )
+
+  structure(game, class = 'alicetwist')
+
+}
+
+
