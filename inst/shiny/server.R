0550446feded5a2b8fc3a30d3a3339264d3a2388
msaltieri <michelestefano.altieri@gmail.com>
2017-04-10 16:33:02 +0200

    Initial commit of the game board

--- inst/shiny/server.R
+++ inst/shiny/server.R
@@ -1,5 +1,61 @@
 source("global.R")
 
+# library(alicetwist)
+
 server <- function(input, output, session) {
 
+  # Variables definition
+  values <- reactiveValues(
+    players = NULL,
+    hands = NULL,
+    fingers = NULL,
+    split = NULL,
+    game = NULL,
+    active = NULL
+  )
+
+  # New game button is clicked
+  observeEvent(input$newgame, ignoreNULL = FALSE, {
+    values$players <- 2 # at the moment only 2 players are allowed
+    values$hands <- 2 # at the moment only 2 hands are allowed
+    values$fingers <- as.numeric(input$finger)
+    values$split <- input$split
+    values$game <- new_game(fingers = values$fingers,
+                            split = values$split)
+    values$active <- TRUE
+  })
+
+  # Game representation
+  output$game <- renderUI({
+    values$game
+
+    isolate({
+      div(
+        id = "game-inner",
+        class = ifelse(values$active, "active", "inactive"),
+        div(id = "game-shield"),
+
+        tagList(
+          div(class = "player",
+              lapply(seq(values$hands), function(hand) {
+                player <- 2
+                value <- game_status(values$game)[player, hand]
+                visClass <- ifelse(value == 0, "empty", "active")
+                id <- sprintf("player%s-hand%s",
+                              player,
+                              hand)
+                actionLink(
+                  id,
+                  value,
+                  class = paste("opponent-hand", visClass),
+                  `player` = player,
+                  `hand` = hand
+                )
+              })
+          ),
+          div()
+        )
+      )
+    })
+  })
 }
--- inst/shiny/ui.R
+++ inst/shiny/ui.R
@@ -6,10 +6,8 @@ fluidPage(
 
   # Page header
   tags$head(
-
     # Style
     tags$link(href = "style.css", rel = "stylesheet")
-
   ),
 
   # Header
@@ -27,5 +25,39 @@ fluidPage(
           tags$a(href = "https://github.com/msaltieri/alicetwist",
                  "on GitHub")
           )
+  ),
+
+  # App Body
+  fluidRow(
+    column(4, wellPanel(
+      id = "leftPanel",
+      selectInput("finger",
+                  "Saturation level",
+                  3:7,
+                  5),
+      selectInput("split",
+                  "Is split allowed?",
+                  c("Yes" = TRUE,
+                    "No" = FALSE)),
+      actionButton("newgame",
+                   "New Game",
+                   class = "btn-lg"),
+      hr(),
+      p("Alice Twist is an interactive game in which each player",
+        "has two or more baskets of balls. The goal of the game",
+        "is to get your opponent without balls hitting his baskets",
+        "with one of its own baskets. When your opponent's basket",
+        "gets hit, the number of balls becomes equal to the sum of",
+        "those already present and those contained in the basket",
+        "with which you hit. When you reach the", em("saturation level"),
+        "of the basket, the basket is empty and no longer have",
+        "balls available. If the", em("split"), "is allowed, empty",
+        "baskets are filled automatically by the balls in other baskets.")
+    )),
+    column(8,
+           hidden(div(id = "congrats",
+                      "Good job, you won!")),
+           uiOutput("game")
+    )
   )
 )
